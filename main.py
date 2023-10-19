e4f00a2c6093c37284e7425fb7cc9f01739ebb1d
Hohol <hohol.struz@yandex.ru>
2023-10-19 16:43:48 +0300

    1

---
+++ main.py
@@ -0,0 +1,30 @@
+list_a = []
+while 1 == 1:
+    input_variable = input('Enter the next number: ')
+    if input_variable == '':
+        break
+    else:
+        list_a.append(int(input_variable))
+
+target = int(input('Enter the target number: '))
+
+a = list_a
+print(a)
+
+i = 0
+while a:
+    if i == (len(a)-1):
+        a.pop()
+        i = 0
+
+    elif a[-1] + a[i] == target:
+        final_answer = [(len(a)-1), i]
+        print(final_answer)
+        break
+
+    else:
+        i += 1
+        print('success iteration')
+
+if not a:
+    print('no solutions')
